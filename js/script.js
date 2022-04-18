13d2dff1f8ddc7f4481f006240d0a22118af9b6b
Maria <mari.shumilova.96@mail.ru>
2022-04-18 14:04:26 +0300

    first task

---
+++ js/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// let number = 4.9;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Mari";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "Makar",
+//     age: 2,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['bchf', 'hurbfnj', 7, [], {}];
+// console.log(arr[0]);
+// alert('Жопа');
+
+// const result = confirm('Do you have phone?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Ваша фамилия?', '');
+// answers[2] = prompt('Ваш возраст?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Mari';
+
+// alert(`Кто прочитает, тот лох, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2 +2 * 2 !== '6');
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);      
